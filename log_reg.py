46b26b0047e7ad6c07db57d871a9b82ed461c7bf
koshtony <tonykungu2>
2021-11-10 16:07:56 +0300

    logistic regression

---
+++ log_reg.py
@@ -0,0 +1,10 @@
+from linear_reg import lin_mod
+import pandas as pd
+import statsmodels.api as sm
+import streamlit as st
+class logit(lin_mod): # logistic regression using statsmodel
+    def logit_imp(self):
+        st.info("conversion to dummie")
+        st.write(pd.get_dummies(self.y).head())
+        mod=sm.Logit(pd.get_dummies(self.y).iloc[:,0],self.x).fit()
+        st.write(mod.summary())
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from linear_reg import lin_mod
+from log_reg import logit
+import pandas as pd
 #@st.cache(suppress_st_warning=True)
 def models_(data,col1,col2,col3):
     sel_x=col2.multiselect("X",data.columns)
@@ -8,3 +10,6 @@ def models_(data,col1,col2,col3):
     if model_type=="Linear Regression":
         mod_class=lin_mod(data[sel_x],data[sel_y])
         mod_class.model_imp()
+    elif model_type=="Logistic Regression":
+        log_class=logit(data[sel_x],data[sel_y])
+        log_class.logit_imp()
