41cde009e65c7978adf8bd01c6386ad5d70ee47d
koshtony <tonykungu2>
2021-11-13 02:07:59 +0300

    completed decision tree

--- dec_tree.py
+++ dec_tree.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from sklearn.tree import DecisionTreeClassifier
 import sklearn.metrics as met
-class tree_class:
+from six import StringIO
+from sklearn.tree import export_graphviz
+import pydotplus
+import pandas as pd
+class tree_class: # implements sklearn decision tree
     def __init__(self,x,y,xt,yt):
         self.x=x
         self.y=y
@@ -11,7 +15,21 @@ class tree_class:
     def dec_tree_imp(self):
         clf=DecisionTreeClassifier()
         clf=clf.fit(self.x,self.y)
-        return clf.predict(self.xt)
+        return clf
     @st.cache
     def dec_tree_clfp(self):
-        return met.classification_report(self.yt,self.dec_tree_imp())
+        return met.classification_report(self.yt,self.dec_tree_imp().predict(self.xt))
+    @st.cache
+    def feature_imp(self):
+        model=self.dec_tree_imp()
+        return model.feature_importances_
+    @st.cache
+    def conf_matrix(self):
+        return met.confusion_matrix(self.yt,self.dec_tree_imp().predict(self.xt))
+    @st.cache
+    def vis_tree(self):
+        dot_data = StringIO()
+        export_graphviz(self.dec_tree_imp(), out_file=dot_data,filled=True,
+                 feature_names = list(self.x.columns),class_names=[str(i) for i in pd.unique(self.y)])
+        graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+        graph.write_png('tree.png')
--- tree_models.py
+++ tree_models.py
@@ -2,6 +2,9 @@ from splitter import split_data
 import streamlit as st
 import time
 from dec_tree import tree_class
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
 def t_models(data,col1,col2,col3):
     s_x=col2.multiselect("features",data.columns)
     s_y=col3.selectbox("target",data.columns)
@@ -18,7 +21,22 @@ def t_models(data,col1,col2,col3):
             tree=tree_class(x_tr,y_tr,x_te,y_te)
             time.sleep(0.01)
             prog.progress(i+1)
+        st.info("classes")
+        st.write([clas for clas in pd.unique(y_tr)])
+        fig=plt.figure(figsize=(3,3))
+        plt.bar([imp for imp in range(len(tree.feature_imp()))],tree.feature_imp())
+        plt.xlabel("features")
+        plt.ylabel("scores")
+        plt.title("features importance")
+        st.pyplot(fig)
+        st.info("classification report")
         st.write(tree.dec_tree_clfp())
+        st.info("Confusion matrix")
+        st.write(tree.conf_matrix())
+        st.info("Tree diagram")
+        tree.vis_tree()
+        t_img=Image.open("tree.png")
+        st.image(t_img)
         #st.balloons()
     elif tree_type=="Random Forest":
         pass
