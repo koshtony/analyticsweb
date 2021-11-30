1fa4ae1b0a2b30243d19588e6f35d49b0f35394f
koshtony <tonykungu2>
2021-11-30 10:32:50 +0300

    added estimator slider

--- tree_reg_models.py
+++ tree_reg_models.py
@@ -8,6 +8,7 @@ def t_regressor(data,col1,col2,col3,testsize): # creating tree regression model 
     x_val=col2.multiselect("Features",data.columns)
     y_val=col2.selectbox("Target",data.columns)
     tree_reg_type=col1.radio("models",['Decision Tree',"Random Forest"])
+    est=col2.slider("no of estimators",1,1000)
     x_tr,x_te,y_tr,y_te=split_data(data[x_val],data[y_val],testsize).split_imp()
     col2.info("train set length: "+str(x_tr.shape[0]))
     col3.info("test set length: "+str(x_te.shape[0]))
@@ -28,7 +29,7 @@ def t_regressor(data,col1,col2,col3,testsize): # creating tree regression model 
         st.write("Training--->")
         progress=st.progress(0)
         for i in range(100):
-            forest=forest_reg(x_tr,y_tr,x_te,y_te)
+            forest=forest_reg(x_tr,y_tr,x_te,y_te,est)
             time.sleep(0.01)
             progress.progress(i+1)
         fet_imp_plot(forest.train_mod())
